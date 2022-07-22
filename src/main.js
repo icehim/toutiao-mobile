abdf6184de76be5627b4b1baa9df41be2414589a
之铭 <zhiming.hao@qq.com>
2022-07-22 15:43:51 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//导入全局样式
+import '@/style/index.less'
+//vant
+import '@/utils/vant'
+//导入flexible(给rem设置基准值)
+import 'amfe-flexible'
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
